994769909388a91d75ed11bc49037ced30fc8576
JohnWest <johnwest@umich.edu>
2013-09-21 18:32:27 -0400

    3 functions completed

--- p2.cpp
+++ p2.cpp
@@ -8,8 +8,13 @@
 
 #include <iostream>
 #include "recursive.h"
+#include "p2.h"
 
 static int sum_helper(int sum, list_t list);
+static int product_helper(int product, list_t list);
+static list_t reverse_helper(list_t list, list_t reverseList);
+
+static list_t insert_list_helper(list_t firstBeginning, list_t firstEnding, list_t second, unsigned int n);
 
 int sum(list_t list)
 {
@@ -24,4 +29,66 @@ static int sum_helper(int sum, list_t list)
     }
 
     return sum_helper(list_first(list) + sum, list_rest(list));
+}
+
+int product(list_t list)
+{
+    return product_helper(1, list);
+}
+
+static int product_helper(int product, list_t list)
+{
+    if (list_isEmpty(list))
+    {
+        return product;
+    }
+    
+    return product_helper(list_first(list) * product, list_rest(list));
+}
+
+int accumulate(list_t list, int (*fn)(int, int), int identity)
+{
+    
+    return 0;
+}
+
+list_t reverse(list_t list)
+{
+    list_t reverseList = list_make();
+
+    return reverse_helper(list, reverseList);
+}
+
+static list_t reverse_helper(list_t list, list_t reverseList)
+{
+    if (list_isEmpty(list)) {
+        return reverseList;
+    }
+
+    return reverse_helper(list_rest(list), list_make(list_first(list), reverseList));
+}
+
+list_t insert_list(list_t first, list_t second, unsigned int n)
+{
+    if (list_isEmpty(first)) {
+        return second;
+    }
+    else if (list_isEmpty(second)) {
+        return first;
+    }
+
+    return insert_list_helper(first, list_make(), second, n);
+}
+
+static list_t insert_list_helper(list_t firstBeginning, list_t firstEnding, list_t second, unsigned int n)
+{
+    if (n == 1) {
+
+    }
+    return second;
+}
+
+list_t chop(list_t l, unsigned int n)
+{
+    return l;
 }
--- p2TestCase.cpp
+++ p2TestCase.cpp
@@ -11,9 +11,79 @@
 #include "p2.h"
 #include "recursive.h"
 
+using namespace std;
+
 int main()
 {
+    int totalCases = 0;
+    int casesPassed = 0;
+
+    int sum_result1 = 0;
+    int sum_result2 = 0;
+    int sum_result3 = 0;
+
+    int sum_edge1 = 0;
+    int sum_edge2 = 0;
+
+    list_t standardList1 = list_make();
+    list_t standardList2 = list_make();
+    list_t standardList3 = list_make();
     
+    list_t edgeList1 = list_make();
+    list_t edgeList2 = list_make(0, list_make());
+
+    for (int i = 4; i > 0; i--) {
+        standardList1 = list_make(i, standardList1);
+        standardList2 = list_make(5 - i, standardList2);
+        standardList3 = list_make(i + 1, standardList3);
+    }
+    // standardList1 is (1,2,3,4)
+    // standardList2 is (4,3,2,1)
+    // standardList3 is (2,3,4,5)
+
+    cout << "Starting test cases for p2..." << endl << "-----------------------------" << endl;
+
+    sum_result1 = sum(standardList1);
+    totalCases++;
+    cout << ".";
+
+    sum_result2 = sum(standardList2);
+    totalCases++;
+    cout << ".";
+
+    sum_result3 = sum(standardList3);
+    totalCases++;
+    cout << ".";
+
+    if (sum_result1 == 10) {
+        casesPassed++;
+    }
+
+    if (sum_result2 == 10) {
+        casesPassed++;
+    }
+
+    if (sum_result3 == 14) {
+        casesPassed++;
+    }
+
+    sum_edge1 = sum(edgeList1);
+    totalCases++;
+    cout << ".";
+
+    sum_edge2 = sum(edgeList2);
+    totalCases++;
+    cout << ".";
+
+    if (sum_edge1 == 0) {
+        casesPassed++;
+    }
+
+    if (sum_edge2 == 0) {
+        casesPassed++;
+    }
+
+    cout << "\nTest Cases passed: " << casesPassed << "/" << totalCases << endl;
 
     return 0;
 }
